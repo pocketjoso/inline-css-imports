029221b502a29278e7bbea99998703eb7bb0ea48
Jonas Ohlsson <jonas.sebastian.ohlsson@gmail.com>
2015-11-21 15:28:32 +0000

    rewrite core for improved parallell import handling
    now requests imports in same file in parellell, rather than in sequence.
    Also resolves a bug where relative import paths were resolved incorrectly
    when css had multiple nested import branches.

--- src/index.js
+++ src/index.js
@@ -1,46 +1,60 @@
 'use strict'
-// adapted from https://github.com/remy/inliner/blob/master/lib/css.js#L39-L76
+// partially adapted from https://github.com/remy/inliner/blob/master/lib/css.js#L39-L76
 
 import fetch from 'node-fetch'
 import url from 'url'
 
-const SAFETY_BREAK_THRESHOLD = 100
-let safeBreakIterator = 0
+const IMPORT_PATTERN = /@import ([^;]*;?)/
+const IMPORT_PATTERN_GLOBAL = new RegExp(IMPORT_PATTERN.source, 'g')
+
+const _cssFromImport = function (importString, baseUrl) {
+  const parts = importString.replace(/url/, '')
+    // {
+    .replace(/['}"()]/g, '')
+    .replace(/;$/, '')
+    .trim()
+    .split(' ') // clean up
+  if (parts.length > 2) {
+    console.error('something wrong, parts length should be no greater than 2', parts.length)
+  }
+  const importHref = parts[0]
+  const mediaTypes = parts.slice(1)
+
+  // ensure import url is absolute
+  const importUrl = url.resolve(baseUrl, importHref)
+
+  return fetch(importUrl)
+  .then(response => response.text())
+  .then(importedCss => {
+    importedCss = importedCss.replace(/\n$/, '')
+
+    if (mediaTypes.length !== 0) {
+      importedCss = `@media ${mediaTypes.join(' ')} { ${importedCss} }`
+    }
+
+    return inlineCssImports(importedCss, importUrl)
+  }).catch(console.log)
+}
 
 export default function inlineCssImports (css, baseUrl) {
-  if (css.indexOf('@import') !== -1) {
-    const match = (css.match(/@import\s*([^;]*;?)/) || [null, ''])[1]
-    const parts = match.replace(/url/, '')
-      .replace(/['}"()]/g, '')
-      .replace(/;$/, '')
-      .trim()
-      .split(' ') // clean up
-    if (parts.length > 2) {
-      console.error('something wrong, parts length should be no greater than 2', parts.length)
-    }
-    const importHref = parts[0]
-    const mediaTypes = parts.slice(1)
-    // ensure import url is absolute
-    const importUrl = url.resolve(baseUrl, importHref)
-
-    return fetch(importUrl)
-    .then(response => response.text())
-    .then(importedCss => {
-      importedCss = importedCss.replace(/\n$/, '')
-      if (mediaTypes.length !== 0) {
-        importedCss = `@media ${mediaTypes.join(' ')} { ${importedCss} }`
-      }
-      // inline into original css
-      css = css.replace(`@import ${match}`, importedCss)
-      safeBreakIterator++
-      if (safeBreakIterator < SAFETY_BREAK_THRESHOLD) {
-        return inlineCssImports(css, importUrl)
-      } else {
-        console.error('too much nesting, aborting..')
-        return css
-      }
-    }).catch(console.log)
-  } else {
+  if (css.indexOf('@import') === -1) {
     return Promise.resolve(css)
   }
+
+  // get promises for css corresponding to each @import declaration in this css
+  let importCssPromises = []
+  let match
+  while ((match = IMPORT_PATTERN_GLOBAL.exec(css)) !== null) {
+    importCssPromises.push(_cssFromImport(match[1], baseUrl))
+  }
+
+  // wait for all importCssPromises to resolve, then inline them into the css
+  return Promise.all(importCssPromises).then(resolvedCssImports => {
+    while (css.indexOf('@import') !== -1) {
+      const match = (css.match(IMPORT_PATTERN) || [null, ''])[1]
+      css = css.replace(`@import ${match}`, resolvedCssImports.shift())
+    }
+    // and we're done!
+    return Promise.resolve(css)
+  })
 }
